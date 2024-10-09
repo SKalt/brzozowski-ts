921295887730e2ef728ed1fb708a4414874a5dbc
Steven Kalt <kalt.steven@gmail.com>
2024-10-08 21:23:05 -0400

    chore: update test script to match dir structure

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 # must be run in repo root
 set -euo pipefail
-tsc --noEmit ./src/test/ok/*.ts
-echo "ok tests passed"
-# FIXME: validate that all the expected failures are present
-tsc --noEmit ./src/test/xfail/*.ts || true
+tsc --noEmit ./test/*.ts
